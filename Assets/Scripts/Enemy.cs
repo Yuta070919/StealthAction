e98d871686c2e56f471049451d1919ea0ed9f62b
yuta <>
2023-05-12 19:57:17 +0900

    first commit

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Enemy : MonoBehaviour
+{
+    public GameObject[] routepoints;
+    public GameObject EnemyObject;
+    public int pointnumber = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RouteMove(routepoints, pointnumber,EnemyObject);
+        pointnumber = PointCheck(routepoints, pointnumber, EnemyObject);
+    }
+    //敵の巡回移動の追加
+    //敵の巡回ポイントへ移動
+    static void RouteMove(GameObject[] routepoints,int pointnumber,GameObject EnemyObject)
+    {
+        EnemyObject.transform.LookAt(routepoints[pointnumber].transform.position);
+        EnemyObject.transform.Translate(0, 0, 1 * Time.deltaTime);
+    }
+    //敵の巡回ポイントに到達したか確認(到達したら＋１)
+    static int PointCheck(GameObject[] routepoints,int pointnumber,GameObject EnemyObject)
+    {
+        if (Vector3.Distance(EnemyObject.transform.position,
+            routepoints[pointnumber].transform.position) <= 1)
+        {
+            pointnumber++;
+            if (pointnumber >= routepoints.Length)
+            {
+                pointnumber = 0;
+                return pointnumber;
+            }
+            else
+            {
+                return pointnumber;
+            }
+        }
+        else
+        {
+            return pointnumber;
+        }
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//using UnityEngine.UI;
+
+public class Player : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(-1 * Time.deltaTime, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(1 * Time.deltaTime, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(0, 0, 1 * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(0, 0, -1 * Time.deltaTime);
+        }
+    }
+}
