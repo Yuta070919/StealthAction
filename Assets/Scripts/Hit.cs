4b386ed536098868e6eaa79a287ea11c43203bed
yuta <>
2023-06-30 20:02:39 +0900

    Hpバーの追加

---
+++ Assets/Scripts/Hit.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hit : MonoBehaviour
+{
+    public Enemy enemy;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void OnTriggerEnter(Collider other)
+    {
+        //プレイヤーが索敵範囲に入ったらEnemy.csにPlayerを追いかけるように関数引き出し
+        if (other.gameObject.name == "Player")
+        {
+            enemy.Phase = EnemyPhase.Tracking;
+        }
+    }
+    public void OnTriggerExit(Collider other)
+    {
+        //プレイヤーが索敵範囲に入ったらEnemy.csに巡回するように関数引き出し
+        if (other.gameObject.name == "Player")
+        {
+            enemy.Phase = EnemyPhase.Patroll;
+        }
+    }
+}
---
+++ Assets/Scripts/HpSlider.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HpSlider : MonoBehaviour
+{
+    public GameObject Enemy;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        try
+        {
+            transform.position = Enemy.transform.position + new Vector3(0, 2, 0);
+        }catch(MissingReferenceException)
+        {
+            Destroy(transform.gameObject);
+        }
+    }
+}
